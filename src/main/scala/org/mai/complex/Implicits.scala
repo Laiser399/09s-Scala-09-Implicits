ba830c0709207acaabefbc87b349cb0962d9074a
Laiser399 <Laiser399@mail.ru>
2021-11-27 18:40:41 +0300

    implementation

--- src/main/scala/org/mai/complex/Implicits.scala
+++ src/main/scala/org/mai/complex/Implicits.scala
@@ -1,16 +1,63 @@
 package org.mai.complex
 
-case class Complex[A: Arithmetic](re: A, im: A) {
+import scala.language.implicitConversions
+
+case class Complex[A: Arithmetical](re: A, im: A) {
   override def toString: String = s"$re+${im}i"
 
-  def +(that: Complex[A]) = null
-  def -(that: Complex[A]) = null
+  val arithmetical: Arithmetical[A] = implicitly[Arithmetical[A]]
+
+  def +(that: Complex[A]): Complex[A] = Complex(
+    arithmetical.add(re, that.re),
+    arithmetical.add(im, that.im)
+  )
+
+  def -(that: Complex[A]): Complex[A] = Complex(
+    arithmetical.subtract(re, that.re),
+    arithmetical.subtract(im, that.im)
+  )
 }
 
-trait Arithmetic[A] {
-
+trait Arithmetical[A] {
+  def add(first: A, second: A): A
+  def subtract(first: A, second: A): A
+  def zero(): A
 }
 
 object Implicits {
+  implicit object DoubleArithmetical extends Arithmetical[Double] {
+    override def add(first: Double, second: Double): Double = first + second
 
+    override def subtract(first: Double, second: Double): Double = first - second
+
+    override def zero(): Double = 0d
+  }
+
+  implicit object IntArithmetical extends Arithmetical[Int] {
+    override def add(first: Int, second: Int): Int = first + second
+
+    override def subtract(first: Int, second: Int): Int = first - second
+
+    override def zero(): Int = 0
+  }
+
+  implicit def tuple2ToComplex[A: Arithmetical](arg: (A, A)): Complex[A] =
+    Complex(arg._1, arg._2)
+
+  implicit def arithmeticalToComplex[A: Arithmetical](arg: A): Complex[A] = {
+    val arithmetical = implicitly[Arithmetical[A]]
+    Complex(arg, arithmetical.zero())
+  }
+
+  implicit class ComplexExtensions[A: Arithmetical](arg: A) {
+    private val arithmetical = implicitly[Arithmetical[A]]
+
+    def real(): Complex[A] = {
+      Complex(arg, arithmetical.zero())
+    }
+
+    def imaginary(): Complex[A] = {
+      Complex(arithmetical.zero(), arg)
+    }
+  }
 }
